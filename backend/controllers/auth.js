3ca567ab2c1f96be07a597b7a5aad4531ee0f2a2
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-07 22:51:56 +0530

    second commit

---
+++ backend/controllers/auth.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcryptjs')
+const register = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+const login = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+const logout = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+module.exports =  {register,login,logout}
