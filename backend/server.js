4011cfceb6a7d33e0cf975b2b6234b108e093f1b
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-08 22:31:05 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,20 @@ const port = 9000;
 const mongoose = require('mongoose')
 const cors = require('cors')
 const cookieparser = require('cookie-parser')
+const authrouter = require('./routes/auth')
+require('dotenv').config();
+app.use(express.json());
+app.use(cookieparser())
+app.use(cors())
+const connection = async(req,res)=>{
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        console.log('db is connected...')
+    } catch (error) {
+        console.error(error);
+    }
+}
+app.use('/auth',authrouter)
+app.listen(port,() => {
+    console.log(`server is running on port ${port}...`)
+})
