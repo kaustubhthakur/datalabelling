7a02607cd3c6ada0e32a7d7b4957ff4220e147cf
Kaustubh <kaustubhthakur76@gmail.com>
2025-01-08 22:25:26 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express();
+const port = 9000;
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieparser = require('cookie-parser')
